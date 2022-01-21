ab240c28d49316732b285fe4cae7a286df8d8b48
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-01-21 11:44:55 -0700

    cheating method

---
+++ lesson1/IntegerBubbleSortCheating.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class IntegerBubbleSortCheating {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 6, 4, 10, 8, 1, 9, 3, 7};
+
+        Arrays.sort(array);
+    }
+}
