bf9b2e71864e17a941eeab5a91d5301cf30ab827
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-01-27 08:08:08 -0700

    Create ArrayListBubbleSort.java

---
+++ semester2/lesson1/ArrayListBubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListBubbleSort {
+	static int sorts = 1;
+
+	public static void sort(ArrayList<Integer> array) {
+		for (int i = 0; i < array.size() - 1; i++) {
+			if (array.get(i) > array.get(i + 1)) {
+				int n1 = array.get(i);
+
+				array.set(i, array.get(i + 1));
+				array.set(i + 1, n1);
+
+				sorts++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> array = new ArrayList<>(List.of(2, 5, 6, 4, 10, 8, 1, 9, 3, 7));
+
+		while (sorts != 0) {
+			sorts = 0;
+			sort(array);
+		}
+	}
+}
