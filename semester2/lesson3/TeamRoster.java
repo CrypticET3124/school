4d5eb1b2a187a9bf64d8a1cc3c5303843867c946
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-02-02 10:12:09 -0700

    Create TeamRoster.java

---
+++ semester2/lesson3/TeamRoster.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class TeamRoster {
+    public static void main(String[] args) {
+		try {
+			Scanner nameS = new Scanner(new File("TeamRosterNames.txt"));
+			Scanner numS = new Scanner(new File("TeamRosterNumbers.txt"));
+			
+			while (nameS.hasNextLine()) {
+				System.out.println(nameS.nextLine() + " - " + numS.nextLine());
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+    }
+}
