88172422546750a48c6b554f88b8aad73cd17858
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-02-14 08:33:21 -0700

    Create Person.java

---
+++ semester2/lesson4/personClass/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+	String fName;
+	String lName;
+	int age;
+	Double height;
+	String eyeColor;
+	String maritalStatus;
+
+	public Person(String fn, String ln, int a, String ms) {
+		fName = fn;
+		lName = ln;
+		age = a;
+		maritalStatus = ms;
+	}
+	
+	public static void main(String[] args) {
+		Person p1 = new Person("John", "Doe", 27, "Single");
+		
+		System.out.println(p1.age);
+	}
+}
