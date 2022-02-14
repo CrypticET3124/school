14097aa34aec1cd21b8cd69dd7238221ce76ed29
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-02-14 08:07:34 -0700

    Create PayrollToFile.java

---
+++ semester2/lesson3/PayrollToFile.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class PayrollToFile {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner(System.in);
+
+		// Name
+		System.out.println("Enter your name:");
+		String name = in.nextLine();
+		// Rate
+		System.out.println("Enter your hourly rate:");
+		while (!in.hasNextDouble()) {
+			System.out.println("That's not a number.");
+			in.nextLine();
+		}
+		double rate = in.nextDouble();
+		in.nextLine();
+		// Hours
+		System.out.println("Enter the number of hours worked:");
+		while (!in.hasNextDouble()) {
+			System.out.println("That's not a number.");
+			in.nextLine();
+		}
+		double hours = in.nextDouble();
+		in.nextLine();
+
+		PrintWriter pw = new PrintWriter("Payroll.txt");
+
+		pw.println("NAME: " + name);
+		pw.println("RATE: " + rate);
+		pw.println("HOURS: " + hours);
+		pw.println("PAY: " + rate * hours);
+
+		pw.close();
+	}
+}
