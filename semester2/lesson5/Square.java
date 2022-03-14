762591a7ebd5be66e914c60a9c0be4ff044fd835
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-03-14 08:40:48 -0600

    Create Square.java

---
+++ semester2/lesson5/Square.java
@@ -0,0 +1,19 @@
+public class Square {
+	public Double length;
+	
+	public Square(Double l) {
+		this.length = l;
+	}
+
+	public Square(int l) {
+		this.length = (double)l;
+	}
+	
+	public Double getArea() {
+		return this.length * this.length;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
