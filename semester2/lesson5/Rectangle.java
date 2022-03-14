3d124a1246e3556a7044c3fb8ca5222cb338677b
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-03-14 08:40:35 -0600

    Create Rectangle.java

---
+++ semester2/lesson5/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle extends Square{
+	public Double width;
+
+	public Rectangle(Double l, Double w) {
+		super(l);
+
+		this.width = w;
+	}
+
+	public Rectangle(int l, int w) {
+		super(l);
+
+		this.width = (double)w;
+	}
+	
+	public Double getArea() {
+		return this.length * this.width;
+	}
+	
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle(5.5, 6.5);
+
+		System.out.println(r.getArea());
+	}
+}
