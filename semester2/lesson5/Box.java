118df0a8fa0296b6d57e93c8319acf33efe919eb
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-03-16 09:47:28 -0600

    Create Box.java

---
+++ semester2/lesson5/Box.java
@@ -0,0 +1,25 @@
+public class Box extends Rectangle {
+	public Double height;
+
+	public Box(Double l, Double w, Double h) {
+		super(l, w);
+
+		this.height = h;
+	}
+
+	public Box(int l, int w, int h) {
+		super(l, w);
+
+		this.height = (double)h;
+	}
+	
+	public Double getArea() {
+		return (2 * this.height * this.width) + (2 * this.height * this.length) + (2 * this.length * this.width);
+	}
+	
+	public static void main(String[] args) {
+		Box b = new Box(5, 5, 5);
+
+		System.out.println(b.getArea());
+	}
+}
