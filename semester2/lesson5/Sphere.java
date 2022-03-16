7c0defe03dbbbbce8acdd3bdc4c4d231e1e225d1
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-03-16 10:15:59 -0600

    Create Sphere.java

---
+++ semester2/lesson5/Sphere.java
@@ -0,0 +1,17 @@
+public class Sphere extends Circle{
+	public Sphere(Double r) {
+		super(r);
+	}
+	
+	public Sphere(int r) {
+		super(r);
+	}
+	
+	public Double getArea() {
+		return 4 * Math.PI * Math.pow(this.radius, 2); //4πr^2
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
