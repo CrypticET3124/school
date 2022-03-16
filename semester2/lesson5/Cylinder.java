490300d1a6b6f39cad30ab5faf83d9bfbe9d421e
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-03-16 10:09:05 -0600

    Create Cylinder.java

---
+++ semester2/lesson5/Cylinder.java
@@ -0,0 +1,21 @@
+public class Cylinder extends Circle {
+	public Double height;
+	
+	public Cylinder(Double r, Double h) {
+		super(r);
+		this.height = h;
+	}
+	
+	public Cylinder(int r, int h) {
+		super(r);
+		this.height = (double)h;
+	}
+	
+	public Double getArea() {
+		return 2 * Math.PI * this.radius * (this.height + this.radius); // 2πr (h + r)
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
