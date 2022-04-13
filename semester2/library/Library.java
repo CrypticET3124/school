3d93e884b067c1ae591774bc1db8860a3f731c97
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-04-13 10:00:54 -0600

    Update Library.java

--- semester2/library/Library.java
+++ semester2/library/Library.java
@@ -6,19 +6,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
+	public static ArrayList<Book> library = new ArrayList<>();
 	
-
-	public static void main(String[] args) throws FileNotFoundException {
-		File bookFile = new File("books.txt");
-		Scanner bin = new Scanner(bookFile);
-		
-		ArrayList<Book> library = new ArrayList<>();
-		
-		while(bin.hasNextLine()) {
-			Book temp = new Book(bin.next(), bin.next(), bin.next(), bin.nextDouble(), bin.next());
+	public static void refreshLib() {
+		Scanner bin;
+		try {
+			bin = new Scanner(new File("books.txt"));
 			
-			library.add(temp);			
+			while(bin.hasNextLine()) { //Title, Author, Genre, Rating, ISBN
+				Book temp = new Book(bin.next().replace("_", " "), bin.next().replace("_", " "), bin.next().replace("_", " "), bin.nextDouble(), bin.next());
+				
+				library.add(temp);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		refreshLib();
 		
 		for(Book b : library) {
 			System.out.println(b);
