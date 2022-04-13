eb05b50656ce687117d1fa34e02c5fbc94efe843
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-04-13 10:07:33 -0600

    Create Umbreon.java

---
+++ semester2/lesson5/pokemon/Umbreon.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Umbreon extends Pokemon{
+	static Random r = new Random();
+	
+	static int[] base = new int[] {95, 65, 110, 60, 130, 65}
+
+	public Umbreon {
+		super(r.nextInt(25) + 6, Umbreon, r.nextInt(32));
+
+		makeStats(this.base);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
