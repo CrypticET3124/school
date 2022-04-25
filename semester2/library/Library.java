e1dbc558276189cfd69fdd0c1a05e8a53302c409
CrypticET3124 <28419042+CrypticET3124@users.noreply.github.com>
2022-04-25 08:59:20 -0600

    Revert "Update Library.java"
    This reverts commit bea9bce2804a31c0f517130f0ec05094c3c312c5.

--- semester2/library/Library.java
+++ semester2/library/Library.java
@@ -8,70 +8,55 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Library {
-static String workDIR = "semester2\\library\\"; // ! workDIR is the working directory, must be changed each system
+	static String workDIR = "semester2\\library\\"; // ! workDIR is the working directory, must be changed each system
 
-public static final List<Book> lib = new ArrayList<>();
-public static final List<Client> clients = new ArrayList<>();
+	public static final List<Book> lib = new ArrayList<>();
+	public static final List<Client> clients = new ArrayList<>();
 
-private static String attributeSep = "\\[\\"; // ! attribute separator: \[\
+	private static String attributeSep = "\\[\\"; // ! attribute separator
 
-public static void refreshLib() { // load library from file into memory
-try (Scanner bin = new Scanner(new File(workDIR, "books.txt"))) {
-while (bin.hasNextLine()) {
-String[] attr = bin.nextLine().split(Pattern.quote(attributeSep));
+	public static void refreshLib() {
+		try (Scanner bin = new Scanner(new File(workDIR, "books.txt"))) {
+			while (bin.hasNextLine()) {
+				String[] attr = bin.nextLine().split(Pattern.quote(attributeSep));
 
-lib.add(new Book(attr[0], attr[1], attr[2], Double.parseDouble(attr[3]), attr[4]));
-}
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+				lib.add(new Book(attr[0], attr[1], attr[2], Double.parseDouble(attr[3]), attr[4]));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-public static void writeClient(Client c) { // add client to a file using printwriter
-try (PrintWriter pw = new PrintWriter(new File(workDIR, "clients.txt"))) {
-pw.print(c.username + attributeSep + c.fname + attributeSep + c.lname + attributeSep + c.password + attributeSep + c.bday.getYear() + attributeSep
-+ c.bday.getMonthValue() + attributeSep + c.bday.getDayOfMonth() + attributeSep);
-for (String b : c.books) {
-pw.print(b + ".");
-}
-pw.print(System.lineSeparator());
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+	public static void writeClient(Client c) {
+		// add client to a file using printwriter
+		try (PrintWriter pw = new PrintWriter(new File(workDIR, "clients.txt"))) {
+			pw.println(c.username + attributeSep + c.fname + attributeSep + c.lname + attributeSep + c.password + attributeSep + c.bday.getYear() + attributeSep
+					+ c.bday.getMonthValue() + attributeSep + c.bday.getDayOfMonth());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-public static void refreshClients() { // load clients from file into memory
-try (Scanner bin = new Scanner(new File(workDIR, "clients.txt"))) {
-while (bin.hasNextLine()) {
-String[] attr = bin.nextLine().split(Pattern.quote(attributeSep));
+	public static void refreshClients() {
+		try (Scanner bin = new Scanner(new File(workDIR, "clients.txt"))) {
+			while (bin.hasNextLine()) {
+				String[] attr = bin.nextLine().split(Pattern.quote(attributeSep));
 
-String[] Strings = {"-1"};
+				clients.add(new Client(attr[0], attr[1], attr[2], attr[3], Integer.parseInt(attr[4]),
+						Integer.parseInt(attr[5]), Integer.parseInt(attr[6])));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-if (attr.length == 8) {
-Strings = attr[7].split(Pattern.quote("."));
-}
+	public static void main(String[] args) {
+		refreshLib();
 
-List<String> tempBooks = new ArrayList<>();
+		for (Book b : lib) {
+			System.out.println(b);
+		}
 
-for (String s : Strings) {
-tempBooks.add(s);
-}
-
-clients.add(new Client(attr[0], attr[1], attr[2], attr[3], Integer.parseInt(attr[4]),
-Integer.parseInt(attr[5]), Integer.parseInt(attr[6]), tempBooks));
-}
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
-
-public static void main(String[] args) {
-refreshLib();
-
-for (Book b : lib) {
-System.out.println(b);
-}
-
-System.out.println(lib.get(0).getTitle());
-}
+		System.out.println(lib.get(0).getTitle());
+	}
 }
